aef568889508fa0927c3a7c379f7def12226ca98
welcomeglory <godbu2021@naver.com>
2024-04-08 11:38:18 +0900

    datasource

--- spring_boot_sjy_emp_repository/src/test/java/edu/ex/database/DataSourceTest.java
+++ spring_boot_sjy_emp_repository/src/test/java/edu/ex/database/DataSourceTest.java
@@ -1,12 +1,19 @@
 package edu.ex.database;
 
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest//테스트를 위한 웹프로그래밍 환경 구축
 class DataSourceTest {
 
+	//커넥션 풀 객체
+	@Autowired
+	private DataSource dataSource; //커넥션풀
 	
+
 	@Test
 	void testConnection() {
 		
