4dfd99b8a90e2794949300e09877829273bac60f
welcomeglory <godbu2021@naver.com>
2024-04-08 11:50:37 +0900

    injection

--- spring_boot_sjy_emp_repository/src/test/java/edu/ex/database/DataSourceTest.java
+++ spring_boot_sjy_emp_repository/src/test/java/edu/ex/database/DataSourceTest.java
@@ -6,16 +6,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest//테스트를 위한 웹프로그래밍 환경 구축
 class DataSourceTest {
 
 	//커넥션 풀 객체
-	@Autowired
+	@Autowired //주입
 	private DataSource dataSource; //커넥션풀
 	
-
 	@Test
 	void testConnection() {
+		assertNotNull(dataSource);
 		
 
 	}
