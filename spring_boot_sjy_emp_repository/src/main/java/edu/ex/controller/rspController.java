74a49f4ec7cd81739eb571462c576befa419f276
welcomeglory <godbu2021@naver.com>
2024-04-09 17:39:20 +0900

    rsp

--- spring_boot_sjy_emp_repository/src/main/java/edu/ex/controller/rspController.java
+++ spring_boot_sjy_emp_repository/src/main/java/edu/ex/controller/rspController.java
@@ -1,42 +1,8 @@
 package edu.ex.controller;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import edu.ex.service.DeptService;
-import edu.ex.service.EmpService;
-import edu.ex.vo.DeptVO;
-import edu.ex.vo.EmpVO;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.ResponseBody;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.ResponseBody;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.text.ParseException;
 
 @Controller 
 public class rspController {
