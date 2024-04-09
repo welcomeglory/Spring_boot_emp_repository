41e88ee54ba6c92b435a10f266112e7f58c66bdb
welcomeglory <godbu2021@naver.com>
2024-04-09 12:37:53 +0900

    vo test

---
+++ spring_boot_sjy_emp_repository/src/test/java/edu/ex/vo/EmpVOTest.java
@@ -0,0 +1,20 @@
+package edu.ex.vo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class EmpVOTest {
+
+	@Test
+	void testEmpVO() {
+		EmpVO emp = new EmpVO(1234,"Charles","manager",9000,null,900,0,12);
+		//주소 나오는거 확인
+		System.out.println(emp);
+
+		assertNotNull(emp);
+		assertEquals(emp.getEmpno(), 1234);
+	}
+
+}
