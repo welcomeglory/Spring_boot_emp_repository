bbfb86eae9e97fe6d5193181dcc3c8b1a04cb8ec
welcomeglory <godbu2021@naver.com>
2024-04-09 12:37:38 +0900

    vo

--- spring_boot_sjy_emp_repository/src/main/java/edu/ex/vo/EmpVO.java
+++ spring_boot_sjy_emp_repository/src/main/java/edu/ex/vo/EmpVO.java
@@ -1,20 +1,24 @@
 package edu.ex.vo;
 
+import java.sql.Timestamp;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class EmpVO {
 	private int empno;
 	private String ename;
 	private String job;
 	private int mgr;
-	private String hiredate;
+	private Timestamp hiredate;
 	private int sal;
 	private int comm;
 	private int deptno;
