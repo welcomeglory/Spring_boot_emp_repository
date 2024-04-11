00f53bdbd1fbc4b5d34348f30020c3d3c3f3e0ef
welcomeglory <godbu2021@naver.com>
2024-04-11 15:48:28 +0900

    add

--- spring_boot_sjy_emp_repository/src/main/java/edu/ex/SpringBootSjyEmpRepositoryApplication.java
+++ spring_boot_sjy_emp_repository/src/main/java/edu/ex/SpringBootSjyEmpRepositoryApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//마이바티스용 인터페이스를 통해 객체생성하기 위한
+//인터페이스 위치를 알려줌.
 @MapperScan(value = {"edu.ex.mapper"})
 @SpringBootApplication
 public class SpringBootSjyEmpRepositoryApplication {
--- spring_boot_sjy_emp_repository/src/main/java/edu/ex/mapper/TimeMapper.java
+++ spring_boot_sjy_emp_repository/src/main/java/edu/ex/mapper/TimeMapper.java
@@ -3,7 +3,7 @@ package edu.ex.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
-//interface안의 함수는 자식에서 구현되어야 하는데
+//인터페이스 클래스 함수는 자식에서 구현되어야 하는데
 //마이바티스는 xml을 통해서 구현하도록 만들어짐.
 @Mapper
 public interface TimeMapper {
