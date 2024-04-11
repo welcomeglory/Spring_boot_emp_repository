9080eb395bbc0ba06ae245593f1321b9d0329d99
welcomeglory <godbu2021@naver.com>
2024-04-11 09:34:22 +0900

    lotto add

--- spring_boot_sjy_emp_repository/src/main/java/edu/ex/controller/LottoController.java
+++ spring_boot_sjy_emp_repository/src/main/java/edu/ex/controller/LottoController.java
@@ -1,17 +1,25 @@
 package edu.ex.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import edu.ex.service.LottoService;
+
 @Controller 
 @RequestMapping("/lotto")
 public class LottoController {
 	
+	@Autowired
+	private LottoService lottoService;
+	
 	@GetMapping("/start")
 	public String start(Model model) {
 		System.out.println("start()..");
+		model.addAttribute("lotto",lottoService.start());
+		
 		return "/lotto/lotto";
 	}
 	
