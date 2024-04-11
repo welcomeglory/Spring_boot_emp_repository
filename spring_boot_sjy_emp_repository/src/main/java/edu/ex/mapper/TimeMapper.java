e8e69793563c42ce9728da770d354a1ba96c9dfe
welcomeglory <godbu2021@naver.com>
2024-04-11 12:29:57 +0900

    TimeMapper

---
+++ spring_boot_sjy_emp_repository/src/main/java/edu/ex/mapper/TimeMapper.java
@@ -0,0 +1,12 @@
+package edu.ex.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface TimeMapper {
+	
+	@Select("select sysdate from dual")
+	public String getTime();	
+	
+}
