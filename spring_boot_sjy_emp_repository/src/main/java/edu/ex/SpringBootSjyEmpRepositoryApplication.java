597d34eea5327fa151658b2d85e1c323b6608e50
welcomeglory <godbu2021@naver.com>
2024-04-11 12:25:42 +0900

    mapper scan

--- spring_boot_sjy_emp_repository/src/main/java/edu/ex/SpringBootSjyEmpRepositoryApplication.java
+++ spring_boot_sjy_emp_repository/src/main/java/edu/ex/SpringBootSjyEmpRepositoryApplication.java
@@ -1,8 +1,10 @@
 package edu.ex;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value = {"edu.ex.mapper"})
 @SpringBootApplication
 public class SpringBootSjyEmpRepositoryApplication {
 
