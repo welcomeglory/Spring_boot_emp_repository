38f65dbe39f41f426340ee5bbad7d31f5f1ca7a8
welcomeglory <godbu2021@naver.com>
2024-04-11 14:58:30 +0900

    TimeMapper.xml 추가

--- spring_boot_sjy_emp_repository/src/test/java/edu/ex/mapper/TimeMapperTest.java
+++ spring_boot_sjy_emp_repository/src/test/java/edu/ex/mapper/TimeMapperTest.java
@@ -2,11 +2,13 @@ package edu.ex.mapper;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import edu.ex.vo.DeptVO;
+import edu.ex.vo.EmpVO;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -20,12 +22,12 @@ class TimeMapperTest {
 	void testTimeMapper() {
 		assertNotNull(timeMapper);
 	}
-	
+	@Disabled
 	@Test
 	void testGetTime() {
 		log.info("시간이 나올까?=========="+timeMapper.getTime());
 	}
-	
+	@Disabled
 	@Test
 	void testGetOnePlusOne() {
 		log.info("1 + 1 = "+timeMapper.getOnePlusOne());
